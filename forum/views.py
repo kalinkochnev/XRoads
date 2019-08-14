db48d019f6a46999cffa35414d1013d4a2552d32
smt5541 <smt5541@rit.edu>
2019-08-13 20:20:11 -0400

    Functional Commit
    Restyled Upvote, Downvote, and Report Buttons (still look bad)
    Made Upvote and Downvote Buttons work (but they work infinite times per user. Valve pls fix)

--- forum/views.py
+++ forum/views.py
@@ -32,21 +32,54 @@ def show_post(request):
         comments = Comment.objects.all().filter(post=id)
         return render(request, 'forum/post.html', {'post': post, 'comments': comments})
     elif request.method == 'POST':
-        message = request.POST.get('message')
-        post = request.POST.get('post')
-        new_comment = Comment(
-            post=Post.objects.get(id=post),
-            user=request.user,
-            text=message,
-            up_votes=0,
-            down_votes=0
-        )
-        if message != "":
-            new_comment.save()
+        for key in request.POST:
+            print(key)
+            value = request.POST[key]
+            print(value)
+        if 'message' in request.POST:
+            message = request.POST.get('message')
+            post = request.POST.get('post')
+            new_comment = Comment(
+                post=Post.objects.get(id=post),
+                user=request.user,
+                text=message,
+                up_votes=0,
+                down_votes=0
+            )
+            if message != "":
+                new_comment.save()
+                return redirect('/post/?id=' + post)
+            else:
+                messages.warning(request, "The Comment cannot be Blank! Please try again.")
+                return redirect('/post/?id=' + post)
+        elif 'uv-comment' in request.POST:
+            commentid = request.POST.get('uv-comment')
+            comment = Comment.objects.filter(id=commentid)
+            uv = comment.get(id=commentid).up_votes
+            Comment.objects.filter(id=commentid).update(up_votes=(uv+1))
+            post = request.POST.get('post')
             return redirect('/post/?id=' + post)
-        else:
-            messages.warning(request, "The Comment cannot be Blank! Please try again.")
+        elif 'dv-comment' in request.POST:
+            commentid = request.POST.get('dv-comment')
+            comment = Comment.objects.filter(id=commentid)
+            dv = comment.get(id=commentid).down_votes
+            Comment.objects.filter(id=commentid).update(down_votes=(dv + 1))
+            post = request.POST.get('post')
             return redirect('/post/?id=' + post)
+        elif 'uv-post' in request.POST:
+            postid = request.POST.get('uv-post')
+            post = Post.objects.filter(id=postid)
+            uv = post.get(id=postid).up_votes
+            Post.objects.filter(id=postid).update(up_votes=(uv + 1))
+            post = request.POST.get('post')
+            return redirect('/post/?id=' + postid)
+        elif 'dv-post' in request.POST:
+            postid = request.POST.get('dv-post')
+            post = Post.objects.filter(id=postid)
+            dv = post.get(id=postid).down_votes
+            Post.objects.filter(id=postid).update(down_votes=(dv + 1))
+            post = request.POST.get('post')
+            return redirect('/post/?id=' + postid)
     else:
         return redirect('home')
 
