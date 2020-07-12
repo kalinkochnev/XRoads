f6f95ef9bba80acb8598ad9a7fa9f50f3184b444
kalinkochnev <kalin.kochnev@gmail.com>
2020-07-12 09:06:11 -0400

    comitted files: created basic login and signup page

--- xroads-react/src/Account.js
+++ xroads-react/src/Account.js
@@ -38,24 +38,33 @@ class LoginPage extends React.Component {
         <form class="accountForm">
           <div class="fields">
             <input class="first-field" type="email" name="email-address" placeholder="Email Address"/>
-            <input type="tel" name="phone-number" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" placeholder="Phone number"/>
-            <input type="text" name="first-name" placeholder="First name"/>
-            <input type="text" name="last-name" placeholder="Last name"/>
             <input class="last-field" type="password" name="password" placeholder="Password"/>
           </div>
           
-          <input id="account-submit" type="submit" value="Submit"/>
+          <input id="account-submit" type="submit" value="Login"/>
         </form>
       </div>
     );
   }
 }
+
+// TODO break up the signup page into several parts for better user experience
 class SignupPage extends React.Component {
 
   render() {
     return (
-      <div class="center-in-pg">
-        <h1>Testing Signup page</h1>
+      <div class="accountLayout">
+        <form class="accountForm">
+          <div class="fields">
+            <input class="first-field" type="email" name="email-address" placeholder="Email Address"/>
+            <input type="tel" name="phone-number" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" placeholder="Phone number"/>
+            <input type="text" name="first-name" placeholder="First name"/>
+            <input type="text" name="last-name" placeholder="Last name"/>
+            <input type="password" name="password" placeholder="Password"/>
+            <input class="last-field" type="password" name="password" placeholder="Confirm Password"/>
+          </div>
+          <input id="account-submit" type="submit" value="sign up"/>
+        </form>
       </div>
     );
   }
