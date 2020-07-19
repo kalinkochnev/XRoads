fd2975cff4a263891d45a1e86e0f4599dae61afb
kalinkochnev <kalin.kochnev@gmail.com>
2020-07-19 12:26:48 -0400

    setup simple dj-rest-auth

--- xroads_django/XroadsAPI/urls.py
+++ xroads_django/XroadsAPI/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('api/', include('XroadsAPI.urls')),
-    path('admin/', admin.site.urls),
-    path('dj-rest-auth/', include('dj_rest_auth.urls'))
+    path('csrf/', views.csrf),
 ]
--- xroads_django/XroadsAPI/views.py
+++ xroads_django/XroadsAPI/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def csrf(request):
+    return render(request, template_name='home.html')
