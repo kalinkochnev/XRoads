a2eb7680240bea7d9ff3ebcb227e5bf3ce9b7d96
kalinkochnev <kalin.kochnev@gmail.com>
2020-07-19 11:08:19 -0400

    no migrations initial django commit

---
+++ xroads_django/scripts/dev-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Make sure to run     chmod a+x install.sh     before running
+# To execute  ./install.sh
+
+sudo apt install python3-pip
+pip3 --version
+pip install -r requirements.txt
+
+sudo apt update
+sudo apt install postgresql postgresql-contrib
+
+password="PTUvEj9Bh9P2"
+sudo -u postgres psql << EOF
+    create database xroadsdb;
+    create user djangouser with encrypted password '$password';
+    alter user djangouser with encrypted password '$password';
+    grant all privileges on database xroadsdb to djangouser;
