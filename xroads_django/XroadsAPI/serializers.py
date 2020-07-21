b5ca0703bdc4c49b2f323cdd937913d22b8b1dec
kalinkochnev <kalin.kochnev@gmail.com>
2020-07-21 15:20:00 -0400

    serializers: Tests for anonymous serializer passed

--- xroads_django/XroadsAPI/serializers.py
+++ xroads_django/XroadsAPI/serializers.py
@@ -3,39 +3,28 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from rest_framework import serializers
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profile
-        fields = ['id', 'email', 'first_name', 'last_name']
-
 class ProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
-
     class Meta:
         model = Profile
-        fields = ['id', 'user', 'is_anon', 'phone']
-        allow_null=True
+        fields = ['id', 'email', 'first_name', 'last_name', 'is_anon', 'phone_num']
+        allow_null = True
 
 class MeetingDaysSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ['day']
 
-class AnonProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
-
+class AnonProfileSerializer(ProfileSerializer):
     class Meta:
         model = Profile
-        fields = ['id', 'user', 'is_anon', 'phone']
-        allow_null=True
+        fields = ['id', 'email', 'first_name', 'last_name', 'is_anon', 'phone_num']
+        allow_null = True
 
     def to_representation(self, instance):
         rep = super().to_representation(instance)
 
         # Removes all anonymous users information from serialization
-        for i, member in enumerate(rep['members']):
-            if member['is_anon']:
-                rep['members'][i] = {'is_anon': True}
-
+        if rep['is_anon']:
+            return {'is_anon': True}
         return rep
 
 class SlideSerializer(serializers.ModelSerializer):
--- xroads_django/XroadsAPI/tests/test_serializers.py
+++ xroads_django/XroadsAPI/tests/test_serializers.py
@@ -1,20 +1,63 @@
 from django.test import TestCase
 from XroadsAPI.models import Profile
-from XroadsAPI.serializers import UserSerializer
+from XroadsAPI.serializers import ProfileSerializer, AnonProfileSerializer
 from XroadsAPI.tests.test_models import TestProfileModel
-class TestUserSerializer(TestCase):
+class TestProfileSerializer(TestCase):
     def test_serialization(self):
+        user_obj:Profile = Profile.objects.create_user(email="a@email.com", password="password", first_name="a", last_name="b", phone="1234567899", is_anon=True)
+        expected = {
+            'id': user_obj.id,
+            'email': user_obj.email,
+            'first_name': user_obj.first_name,
+            'last_name': user_obj.last_name,
+            'is_anon': user_obj.is_anon,
+            'phone_num': user_obj.phone_num,
+        }
+
+        assert expected == ProfileSerializer(user_obj).data
+
+    def test_optional_fields(self):
         user_obj = Profile.objects.create_user(email="a@email.com", password="password", first_name="a", last_name="b")
         expected = {
             'id': user_obj.id,
             'email': user_obj.email,
             'first_name': user_obj.first_name,
-            'last_name': user_obj.last_name
+            'last_name': user_obj.last_name,
+            'is_anon': user_obj.is_anon,
+            'phone_num': None
         }
 
-        assert expected == UserSerializer(user_obj).data
+        assert expected == ProfileSerializer(user_obj).data
 
+class TestAnonProfileSerializer(TestCase):
+    def generate_test_profiles(self, num):
+        visible = []
+        invisible = []
+        for i in range(num):
+            if i % 2 == 0:
+                visible.append(TestProfileModel.create_test_prof(num))
+            else:
+                invisible.append(TestProfileModel.create_test_prof(num))
+        return visible, invisible
 
-class TestProfileSerializer(TestCase):
-    def test_blank_fields_allowed(self):
-        pass
+    def test_remove_anon_profiles(self):
+        prof1 = TestProfileModel.create_test_prof(1, is_anon=True)
+        expected = {
+            'is_anon': True
+        }
+
+        assert AnonProfileSerializer(prof1).data == expected
+    
+    def test_not_anon_serialization(self):
+        prof1 = TestProfileModel.create_test_prof(1)
+        expected = {
+            'id': prof1.id,
+            'email': prof1.email,
+            'first_name': prof1.first_name,
+            'last_name': prof1.last_name,
+            'is_anon': prof1.is_anon,
+            'phone_num': prof1.phone_num,
+        }
+
+        assert AnonProfileSerializer(prof1).data == expected
+        
