4af41d716c5ff136764156b4a7df52077cdab5f2
kalinkochnev <kalin.kochnev@gmail.com>
2020-08-22 11:03:00 -0400

    Styled alert bar

--- xroads-react/src/components/Common/AlertBar/AlertBar.jsx
+++ xroads-react/src/components/Common/AlertBar/AlertBar.jsx
@@ -1,3 +1,21 @@
-let color_type = {
-    'warning': ''.
+import React from 'react';
+
+const AlertBar = (props) => {
+    return (
+        <div className={`${props.alertType}-alert`}>
+            <h2>{props.message}</h2>
+        </div>
+    );
 }
+
+const Alert = (props) => {
+
+    return (
+        <div className={`${props.alertType}-alert`}>
+            <h2>{props.message}</h2>
+        </div>
+    );
+}
+
+
+export {Alert as AlertBar};
--- xroads-react/src/screens/Root.jsx
+++ xroads-react/src/screens/Root.jsx
@@ -6,11 +6,15 @@ import ScreenClubDetail from "./Club/Page";
 import ScreenLogin from "./User/Login";
 import ScreenSignup from "./User/Signup";
 import ScreenNotFound from "./Generic/NotFound";
+import {AlertBar} from '../components/Common/AlertBar/AlertBar';
 
 const ScreensRoot = () => {
   return (
     <Router>
       <Switch>
+        <Route exact path="/">
+          <AlertBar message="Hello there" alertType="danger"></AlertBar>
+        </Route>
         <Route path="/signup" component={ScreenSignup} />
         <Route exact path="/login" component={ScreenLogin} />
         <Route exact path="/clubs" component={ScreenClubBrowser} />
