ff16b73fd987843e5ef17485624e77bfa656899e
kalinkochnev <kalin.kochnev@gmail.com>
2020-08-22 15:28:06 -0400

    added dismissable alerts

--- xroads-react/src/components/Common/AlertBar/AlertBar.jsx
+++ xroads-react/src/components/Common/AlertBar/AlertBar.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, {useState} from "react";
 
 const AlertBar = (props) => {
     return (
-        <div className={`${props.alertType}-alert`}>
-            <h2>{props.message}</h2>
+        <div>{props.children}</div>
+    );
+};
+
+// Same as props.type, props.children
+const Message = ({type, children, dismissable=true}) => {
+    const [dismissed, setDismissed] = useState(false);
+
+    let dismissBtn = (<button className="dismiss-button" onClick={()=>setDismissed(true)}></button>);
+    let msg = (
+        <div className={`${type}-alert`}>
+            <div className="alert-message">
+                <h2>{children}</h2>
+            </div>
+            
+            {dismissable ? dismissBtn : (<div className="empty-dismiss"></div>)}
         </div>
     );
-}
-
-const Alert = (props) => {
-
-    return (
-        <div className={`${props.alertType}-alert`}>
-            <h2>{props.message}</h2>
-        </div>
-    );
+    return !dismissed ? msg : null;
 }
 
 
-export {Alert as AlertBar};
+export { AlertBar, Message };
