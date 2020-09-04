260fe90a69c44fa5251dc086690c23c96c4e9ebc
kalinkochnev <kalin.kochnev@gmail.com>
2020-09-04 09:10:12 -0400

    Fixed search bar icon and width. Made club browser cards more responsive

--- xroads-react/src/components/Common/Navbar/Navbar.jsx
+++ xroads-react/src/components/Common/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Navbar.scss";
 import {removeAuthCookies} from '../../../service/xroads-api'
 import { useHistory } from "react-router-dom";
+import { AlertBar, Message } from "../AlertBar/AlertBar";
 
 const Navbar = () => {
   const history = useHistory();
@@ -11,6 +12,7 @@ const Navbar = () => {
     return history.replace('/login')
   }
   return (
+    <div>
       <div className="navbar-simple">
         <div className="xroadsLogo">
           <h1>xroads</h1>
@@ -19,6 +21,10 @@ const Navbar = () => {
           <button onClick={logout}>logout</button>
         </div>
       </div>
+      <AlertBar>
+        <Message type="info">I AM  T E X T asdk fklsdajf lksdalfk sdalfkljsadjklf askjlsakljf jlksdafalkj sadkljfkj lsdals kjdlkjk jlfkljalk jsadlkj flds kljajklf aslkf lkjas kla</Message>
+      </AlertBar>
+    </div>
     );
 
 }
