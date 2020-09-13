a490be4bfa97bd2f9637578454258cd5e5c0c19c
kalinkochnev <kalin.kochnev@gmail.com>
2020-09-13 11:58:55 -0400

    Fixed bug where react reloads browser page on any user state change

--- xroads-react/src/screens/Club/Browser.jsx
+++ xroads-react/src/screens/Club/Browser.jsx
@@ -55,7 +55,7 @@ const ScreenClubBrowser = () => {
   useEffect(() => {
     console.log("ClubBrowser component did mount");
     loadClubs();
-  }, [state.user])
+  }, [state.user.district, state.user.school])
   
   return (
     <div>
@@ -67,7 +67,7 @@ const ScreenClubBrowser = () => {
             displayedClubs.length == 0 ? (<h1>Loading...</h1>) : 
             displayedClubs.map(club => <ClubCard
               key={club.id}
-              id={3}
+              id={club.id}
               title={club.name}
               imageURL={club.main_img}
               description={club.description}
