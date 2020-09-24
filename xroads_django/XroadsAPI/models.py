69757eda1812dedffb09402400212da413957dc9
kalinkochnev <kalin.kochnev@gmail.com>
2020-09-23 20:25:03 -0400

    Fixed bug with get editors

--- xroads_django/XroadsAPI/models.py
+++ xroads_django/XroadsAPI/models.py
@@ -81,7 +81,9 @@ class Club(models.Model):
     def editors(self):
         import XroadsAuth.permissions as AuthPerms
         club_role = AuthPerms.Role.from_start_model(self)
-        return AuthModels.Profile.objects.filter(roles__in=[AuthModels.RoleModel.objects.get(role_name=club_role.role_str)])
+        # FIXME make sure it works with more than one role model
+        
+        return AuthModels.Profile.objects.filter(roles__role_name=club_role.role_str)
 
 class School(models.Model):
     name = models.CharField(max_length=40)
