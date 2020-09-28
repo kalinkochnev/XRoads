51acb477867ce2914c20d999320c001c36fb1baa
Alex Kochnev <akochnev@gmail.com>
2020-09-28 01:18:01 -0400

    Added cors middleware

--- xroads_django/xroads_django/settings.py
+++ xroads_django/xroads_django/settings.py
@@ -88,7 +88,7 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 MIDDLEWARE = [
-    # 'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
