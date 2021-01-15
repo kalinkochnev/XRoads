88e6a42ce88040c2309138903854c4cc117581ac
rmenon1008 <rmenon1008@gmail.com>
2021-01-14 21:26:37 -0500

    Added Meeting Times Card to Featured. Changed sizing for all screen sizes. Changed search box font.

--- xroads-react/src/components/Club/Featured/Featured.jsx
+++ xroads-react/src/components/Club/Featured/Featured.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { AutoSlide, Slideshow } from "../../Common/Slides/Slides";
+import Sticky from '../../Common/StickyCard/StickyCard';
+import './Featured.scss';
 import ReactPlayer from "react-player";
 
 const FeaturedCard = (props) => {
@@ -13,15 +15,39 @@ const FeaturedCard = (props) => {
         return null;
     }
 
+    var description = club.description;
+    if (club.description.length > 150) {
+        description = club.description.substring(0, 150).concat("...")
+    }
+
     return (
-        <div>
-            <h1>Featured Club!</h1>
-            <h2>{club.name}</h2>
-            <p>{club.description}</p>    
-            <Slideshow>
-                {club.slides != null ? club.slides.map(url => <AutoSlide url={url}></AutoSlide>) : null}
-            </Slideshow>
-            
+        <div class="featured-container">
+            <div className="meetings">
+                <Sticky label="Upcoming Events">
+                    <div className="meeting-list">
+                        <h3>Today</h3>
+                        <p>Robotics Club · 3 pm</p>
+                        <p>Model UN · 3:30 pm</p>
+                        <p>Badminton Club · 4 pm</p>
+
+                        <h3>Upcoming</h3>
+                        <p>Robotics Club · Jan 6 · 4 pm</p>
+                    </div>
+                </Sticky>
+            </div>
+            <div className="featured-club">
+                <Sticky label="Featured Club">
+                    <div className="featured-club-content">
+                        <Slideshow singleSlide>
+                            {club.slides != null ? club.slides.map(url => <AutoSlide url={url}></AutoSlide>) : null}
+                        </Slideshow>
+                        <div className="featured-club-details">
+                            <h2>{club.name}</h2>
+                            <p>{description}</p>
+                        </div>
+                    </div>
+                </Sticky>
+            </div>
         </div>
     );
 }
