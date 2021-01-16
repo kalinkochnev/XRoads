22ec02556eb45980667f8c2094837005946aae75
rmenon1008 <rmenon1008@gmail.com>
2021-01-15 20:33:14 -0500

    Added link to featured card. Fixed overflow on featured card. Added shadow to sticky.

--- xroads-react/src/components/Club/Featured/Featured.jsx
+++ xroads-react/src/components/Club/Featured/Featured.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { AutoSlide, Slideshow } from "../../Common/Slides/Slides";
 import Sticky from '../../Common/StickyCard/StickyCard';
 import './Featured.scss';
+import { Link, useHistory } from "react-router-dom";
 import ReactPlayer from "react-player";
 
 const FeaturedCard = (props) => {
@@ -17,24 +18,12 @@ const FeaturedCard = (props) => {
 
     var description = club.description;
     if (club.description.length > 150) {
-        description = club.description.substring(0, 150).concat("...")
+        description = club.description.substring(0, 5000).concat("...")
     }
 
     return (
         <div class="featured-container">
-            <div className="meetings">
-                <Sticky label="Upcoming Events">
-                    <div className="meeting-list">
-                        <h3>Today</h3>
-                        <p>Robotics Club · 3 pm</p>
-                        <p>Model UN · 3:30 pm</p>
-                        <p>Badminton Club · 4 pm</p>
-
-                        <h3>Upcoming</h3>
-                        <p>Robotics Club · Jan 6 · 4 pm</p>
-                    </div>
-                </Sticky>
-            </div>
+            <MeetingsSummary />
             <div className="featured-club">
                 <Sticky label="Featured Club">
                     <div className="featured-club-content">
@@ -42,14 +31,35 @@ const FeaturedCard = (props) => {
                             {club.slides != null ? club.slides.map(url => <AutoSlide url={url}></AutoSlide>) : null}
                         </Slideshow>
                         <div className="featured-club-details">
-                            <h2>{club.name}</h2>
-                            <p>{description}</p>
+                            <Link to={`/school/${club.school}/clubs/${club.id}`}>
+                                <h2>{club.name}</h2>
+                                <p>{description}</p>
+                                <div className="haze-bottom" />
+                            </Link>
                         </div>
                     </div>
                 </Sticky>
             </div>
         </div>
     );
+}
+
+const MeetingsSummary = (props) => {
+    return (
+        <div className="meetings">
+            <Sticky label="Upcoming Events">
+                <div className="meeting-list">
+                    <h3>Today</h3>
+                    <p>Robotics Club · 3 pm</p>
+                    <p>Model UN · 3:30 pm</p>
+                    <p>Badminton Club · 4 pm</p>
+
+                    <h3>Upcoming</h3>
+                    <p>Robotics Club · Jan 6 · 4 pm</p>
+                </div>
+            </Sticky>
+        </div>
+    )
 }
 
 export default FeaturedCard;
--- xroads-react/src/components/Common/StickyCard/StickyCard.jsx
+++ xroads-react/src/components/Common/StickyCard/StickyCard.jsx
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import './StickyCard.scss';
 
 const Sticky = (props) => {
-  return (
-      <div className="sticky">
-        <div className="sticky-label">
-            <h3>{props.label}</h3>
+    return (
+        <div className="sticky">
+            <div className="sticky-label">
+                <h3>{props.label}</h3>
+            </div>
+            {props.children}
         </div>
-        {props.children}
-      </div>
-  );
+    );
 }
 export default Sticky;
