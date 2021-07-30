2733f08800e81b311222b81c36ecdf215ac9f9b2
kalinkochnev <kalin.kochnev@gmail.com>
2021-07-30 12:35:34 -0400

    Nginx with letsencrypt works

--- backend/compose/production/nginx/entrypoint.sh
+++ backend/compose/production/nginx/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-certbot --nginx -d xroads.club,www.xroads.club --email kalin.kochnev@gmail.com -n --agree-tos --expand
+# certbot --nginx -d xroads.club,www.xroads.club --email kalin.kochnev@gmail.com -n --agree-tos --expand
 
 nginx -g "daemon off;"
