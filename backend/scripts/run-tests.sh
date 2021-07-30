0e9f23e086436f9940d5bb7aff49ed3d9459ab80
rmenon1008 <rmenon1008@gmail.com>
2021-07-30 00:08:01 -0400

    Revert "Dev container now working"
    This reverts commit 60df2c7bd33fb4926adb462cc88afecd93192010.

---
+++ backend/scripts/run-tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Make sure to run     chmod a+x install.sh     before running
+# Make sure to be in the root of the django directory before running!
+
+source $1/bin/activate
+pip install -r requirements.txt
+pytest --cov=backend/ backend/tests/ --count=1
+#pytest --cov-append --cov=XroadsAuth/ XroadsAuth/tests/ --count=1
+coverage html --omit="*/test*"
