889f5159b375f58a742152e670014c571bde88e6
kalinkochnev <kalin.kochnev@gmail.com>
2021-07-29 21:59:39 -0400

    Removed old backend

--- xroads_django/XroadsAPI/tests/test_serializers.py
+++
@@ -1,9 +0,0 @@
-from testutils.conftest import temp_img
-from django.core.files.uploadedfile import SimpleUploadedFile
-
-import tempfile
-from collections import OrderedDict
-import random
-import pytest
-
-from XroadsAPI.serializers import *
