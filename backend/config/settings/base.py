825003d67aab2c68b2ce239748dcfb504304a4f5
kalinkochnev <kalin.kochnev@gmail.com>
2021-07-30 20:36:41 -0400

    Fixing permissions (again)

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -267,7 +267,7 @@ LOGGING = {
 # django-rest-framework - https://www.django-rest-framework.org/api-guide/settings/
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.SessionAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
 }
